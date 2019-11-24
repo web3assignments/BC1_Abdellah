f32c108683806aae545d3382ec4d1436746516a1
Abdellah Choukod <all-ch@hotmail.com>
2019-11-24 22:01:57 +0100

    Exercise extended with feedback and new homework

---
+++ Day_02_Wed_20_11_2019/TicketSystem.sol
@@ -0,0 +1,37 @@
+
+pragma solidity >=0.5.11;
+
+contract TicketSystem {
+
+      struct Ticket {
+        uint id;
+        string eventName;
+        string eventPlace;
+        bool booked;
+    }
+
+    Ticket[] public ticketArray;
+    mapping (address => Ticket) public lastTicketBooked;
+    event ticketIsBooked(address receiver, uint id, string eventName);
+    event ticketIsMade(address receiver, uint id, string eventName);
+
+    function makeTicket(string memory eventName, string memory eventPlace) public {
+        Ticket memory ticket = Ticket(ticketArray.length, eventName, eventPlace, false);
+        ticketArray.push(ticket);
+        emit ticketIsMade(msg.sender, ticket.id, ticket.eventName);
+    }
+
+    function bookTicket(uint id) public {
+        require(id >= 0, "id must be equal or larger than 0");
+        Ticket memory ticket = ticketArray[id];
+        ticket.booked = true;
+        emit ticketIsBooked(msg.sender, ticket.id, ticket.eventName);
+        lastTicketBooked[msg.sender] = ticket;
+    }
+
+    function getLatestTicketBookedDetails() public view returns (uint id, string memory eventName, string memory eventPlace)  {
+        Ticket memory ticket = lastTicketBooked[msg.sender];
+        return (ticket.id, ticket.eventName, ticket.eventPlace);
+    }
+
+}
