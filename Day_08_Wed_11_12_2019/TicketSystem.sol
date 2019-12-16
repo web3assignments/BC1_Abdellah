52f6c1b397fe3100c3c17077fe5fd965e4c64c4c
Abdellah Choukod <all-ch@hotmail.com>
2019-12-16 10:22:00 +0100

    Added two patterns

---
+++ Day_08_Wed_11_12_2019/TicketSystem.sol
@@ -0,0 +1,62 @@
+
+pragma solidity >=0.5.11;
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+contract TicketSystem is usingOraclize  {
+    string name;
+    uint256 public priceOfUrl;
+    constructor() public payable {}
+
+      struct Ticket {
+        uint id;
+        string eventName;
+        string eventPlace;
+        bool booked;
+    }
+
+    uint enabledAt = now;
+    
+    modifier enabledEvery(uint t) {
+    if (now >= enabledAt) {
+      enabledAt = now + t;
+      _;      
+    }
+    }
+    
+    function __callback(bytes32 myid, string memory result ) public {
+        if (msg.sender != provable_cbAddres()) revert();
+        name = result[0].name;
+    }
+
+    Ticket[] public ticketArray;
+    mapping (address => Ticket) public lastTicketBooked;
+    event ticketIsBooked(address receiver, uint id, string eventName);
+    event ticketIsMade(address receiver, uint id, string eventName);
+
+    function makeTicket(string memory eventName, string memory eventPlace) public {
+        Ticket memory ticket = Ticket(ticketArray.length, eventName, eventPlace, false);
+        ticketArray.push(ticket);
+        emit ticketIsMade(msg.sender, ticket.id, ticket.eventName);
+    }
+
+    function bookTicket(uint id) public enabledEvery(1 minutes) {
+        require(id >= 0, "id must be equal or larger than 0");
+        Ticket storage ticket = ticketArray[id];
+        ticket.booked = true;
+        emit ticketIsBooked(msg.sender, ticket.id, ticket.eventName);
+        lastTicketBooked[msg.sender] = ticket;
+    }
+
+    function getLatestTicketBookedDetails() public view returns (uint id, string memory eventName, string memory eventPlace)  {
+        Ticket memory ticket = lastTicketBooked[msg.sender];
+        return (ticket.id, ticket.eventName, ticket.eventPlace);
+    }
+
+    function getRandomTicketOwnerName() public payable {
+         if (oraclize_getPrice("URL") > this.balance) {
+            return "please add some ETH to cover for the query fee";
+        } else {
+            oraclize_query("URL", "json(https://uinames.com/api/?amount=1)");
+        }
+    }
+}
---
+++ Day_08_Wed_11_12_2019/motivation.md
@@ -0,0 +1,7 @@
+I chose for the Oracle pattern & rate limit pattern.
+Because I make use of an orcale, 
+I decided to chose the Oracle pattern, 
+so that the incoming data is handled via a safe manner.
+Besides, I used also the rate limit pattern, because multiple 
+requests at once could affect the performance of the application.
+
